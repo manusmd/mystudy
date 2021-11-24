9a9eb0738eef4519eff22bcbfe2549077bbc392c
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-11-25 00:37:59 +0100

    :recycle: Change story(book) class to inline styling

--- src/Components/InputElement/InputElement.stories.tsx
+++ src/Components/InputElement/InputElement.stories.tsx
@@ -10,7 +10,7 @@ export const Small = () => (
   <InputElement size="small" placeholder="username" type="text" />
 );
 export const Medium = () => (
-  <div className={styles.story}>
+  <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
     <InputElement size="medium" placeholder="username" type="text" />
     <InputElement size="medium" placeholder="password" type="password" />
   </div>
