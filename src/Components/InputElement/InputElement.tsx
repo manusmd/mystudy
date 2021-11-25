2984b3e3b5a7630610390c2147a535218818cf7b
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-11-25 04:02:31 +0100

    :sparkles: Add state to InputElement

--- src/Components/InputElement/InputElement.tsx
+++ src/Components/InputElement/InputElement.tsx
@@ -4,18 +4,21 @@ type InputElementProps = {
   size: string;
   placeholder: string;
   type: string;
+  onChange: (input: string) => void;
 };
 
 export default function InputElement({
   size,
   placeholder,
   type,
+  onChange,
 }: InputElementProps) {
   return (
     <input
       className={`${styles[size]} ${styles.inputElement}`}
       placeholder={placeholder}
       type={type}
+      onChange={(event) => onChange(event.target.value)}
     />
   );
 }
