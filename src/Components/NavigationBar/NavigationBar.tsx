3dfe1d1b95147cd565145fc40d61bb5b67839460
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-11-26 11:34:21 +0100

    :sparkles: Implement basic react router structure

--- src/Components/NavigationBar/NavigationBar.tsx
+++ src/Components/NavigationBar/NavigationBar.tsx
@@ -4,17 +4,58 @@ import TeachersIcon from './assets/Teachers';
 import HomeIcon from './assets/Home';
 import GroupsIcon from './assets/Groups';
 import SubjectsIcon from './assets/Subjects';
+import { Link, BrowserRouter } from 'react-router-dom';
 
-export default function NavigationBar() {
+type NavigationBarProps = {
+  activeLink?: 'students' | 'teachers' | 'home' | 'groups' | 'subjects';
+};
+
+export default function NavigationBar({ activeLink }: NavigationBarProps) {
   return (
-    <div className={styles.navigationBar}>
-      <StudentsIcon />
-      <TeachersIcon />
-      <div className={styles.homeCircle}>
-        <HomeIcon />
-      </div>
-      <GroupsIcon />
-      <SubjectsIcon />
-    </div>
+    <nav className={styles.navigationBar}>
+      <BrowserRouter>
+        <Link to="/students">
+          <StudentsIcon
+            fill={
+              activeLink === 'students'
+                ? 'var(--primary-color)'
+                : 'var(--secondary-text-color)'
+            }
+          />
+        </Link>
+        <Link to="/teachers">
+          <TeachersIcon
+            fill={
+              activeLink === 'teachers'
+                ? 'var(--primary-color)'
+                : 'var(--secondary-text-color)'
+            }
+          />
+        </Link>
+        <Link to="/home">
+          <div className={styles.homeCircle}>
+            <HomeIcon />
+          </div>
+        </Link>
+        <Link to="/groups">
+          <GroupsIcon
+            fill={
+              activeLink === 'groups'
+                ? 'var(--primary-color)'
+                : 'var(--secondary-text-color)'
+            }
+          />
+        </Link>
+        <Link to="/subjects">
+          <SubjectsIcon
+            fill={
+              activeLink === 'subjects'
+                ? 'var(--primary-color)'
+                : 'var(--secondary-text-color)'
+            }
+          />
+        </Link>
+      </BrowserRouter>
+    </nav>
   );
 }
