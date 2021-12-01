b7b30525753f23334ec46e4fd537828909ebe130
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-12-01 10:36:46 +0100

    :sparkles: Add Dashboard page with one smallChart

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,59 @@
 import styles from './Dashboard.module.css';
+import TitleElement from '../../Components/TitleElement/TitleElement';
+import SmallChart from '../../Components/SmallChart/SmallChart';
+import { useEffect, useState } from 'react';
 
-export default function Dashboard() {
-  return <h1 className={styles.title}>Dashboard</h1>;
+type StudentsProps = {
+  id?: number;
+  lastname: string;
+  surname: string;
+  gender: 'male' | 'female' | 'others';
+  address: string;
+  phone: string;
+  groups: string[];
+  subjects: string[];
+};
+export default function Dashboard(): JSX.Element {
+  const [allStudents, setAllStudents] = useState<StudentsProps[]>([]);
+  const [studentData, setStudentData] = useState<number[] | null>(null);
+
+  useEffect(() => {
+    const studentsFetch = async () => {
+      const responseStudents = await fetch(
+        'https://server.manu-web.de/students',
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem('jwt')}`,
+          },
+        }
+      );
+      const studentsBody = await responseStudents.json();
+      setAllStudents(studentsBody);
+    };
+    studentsFetch();
+  }, []);
+
+  const studentCount = allStudents.reduce(
+    (prev, cur) => {
+      const obj = { ...prev };
+      obj[cur.gender]++;
+      return obj;
+    },
+    {
+      male: 0,
+      female: 0,
+      others: 0,
+    }
+  );
+
+  return (
+    <>
+      <TitleElement title="Dashboard" />
+      <SmallChart
+        label={'students'}
+        data={[studentCount.male, studentCount.female, studentCount.others]}
+        labels={['male', 'male', 'others']}
+      />
+    </>
+  );
 }
