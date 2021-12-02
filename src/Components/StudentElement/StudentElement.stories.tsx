75511ca7716646bd723203aba4af3e34e3684f36
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-12-02 11:23:00 +0100

    :sparkles: Add studentElement component files

---
+++ src/Components/StudentElement/StudentElement.stories.tsx
@@ -0,0 +1,8 @@
+import StudentElement from './StudentElement';
+
+export default {
+  component: StudentElement,
+  title: 'Components/StudentElement',
+};
+
+export const Default = () => <StudentElement />;
---
+++ src/Components/StudentElement/StudentElement.tsx
@@ -0,0 +1,5 @@
+import styles from './StudentElement.module.css';
+
+export default function StudentElement() {
+  return <div className={styles.container}>StudentElement</div>;
+}
