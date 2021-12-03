30a55fda0ce74438e2a2cd286c21a3d62f8d9b77
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-12-03 16:44:54 +0100

    :sparkles: Add pull to refresh

--- src/Pages/Students/Students.tsx
+++ src/Pages/Students/Students.tsx
@@ -4,8 +4,25 @@ import fetchAPI from '../../utils/api';
 import styles from './Students.module.css';
 import TitleElement from '../../Components/TitleElement/TitleElement';
 import EntryElement from '../../Components/EntryElement/EntryElement';
+import PullToRefresh from 'pulltorefreshjs';
 
 export default function Students(): JSX.Element {
+  PullToRefresh.init({
+    mainElement: 'div',
+    onRefresh() {
+      const studentsFetch = async () => {
+        const studentsBody = await fetchAPI(
+          'https://server.manu-web.de/students'
+        );
+
+        setAllStudents(studentsBody);
+      };
+      studentsFetch();
+      /*       window.location.reload();
+       */
+    },
+  });
+
   const [allStudents, setAllStudents] = useState<StudentsType[]>([]);
   useEffect(() => {
     const studentsFetch = async () => {
