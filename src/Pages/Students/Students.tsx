84c04bb1e219df7a3b99723652d8609bc3505831
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-12-03 10:41:43 +0100

    :sparkles: Add students fetch

--- src/Pages/Students/Students.tsx
+++ src/Pages/Students/Students.tsx
@@ -1,7 +1,21 @@
-import TitleElement from '../../Components/TitleElement/TitleElement';
+import { useEffect, useState } from 'react';
+import { StudentsType } from '../../utils/types';
+import fetchAPI from '../../utils/api';
 import styles from './Students.module.css';
+import TitleElement from '../../Components/TitleElement/TitleElement';
 
 export default function Students(): JSX.Element {
+  const [allStudents, setAllStudents] = useState<StudentsType[]>([]);
+  useEffect(() => {
+    const studentsFetch = async () => {
+      const studentsBody = await fetchAPI(
+        'https://server.manu-web.de/students'
+      );
+
+      setAllStudents(studentsBody);
+    };
+    studentsFetch();
+  });
   return (
     <>
       <TitleElement title="Students" />
