046bdb5c7e5735ce3180b95f4472f80b0b2fcea3
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-12-03 17:02:38 +0100

    :recycle: Move setfetch to helper.tsx

--- src/Pages/Students/Students.tsx
+++ src/Pages/Students/Students.tsx
@@ -1,38 +1,22 @@
 import { useEffect, useState } from 'react';
 import { StudentsType } from '../../utils/types';
-import fetchAPI from '../../utils/api';
 import styles from './Students.module.css';
 import TitleElement from '../../Components/TitleElement/TitleElement';
 import EntryElement from '../../Components/EntryElement/EntryElement';
 import PullToRefresh from 'pulltorefreshjs';
+import setFetch from '../../utils/helper';
 
 export default function Students(): JSX.Element {
+  const [allStudents, setAllStudents] = useState<StudentsType[]>([]);
   PullToRefresh.init({
     mainElement: 'div',
     onRefresh() {
-      const studentsFetch = async () => {
-        const studentsBody = await fetchAPI(
-          'https://server.manu-web.de/students'
-        );
-
-        setAllStudents(studentsBody);
-      };
-      studentsFetch();
-      /*       window.location.reload();
-       */
+      setFetch('students', setAllStudents);
     },
   });
 
-  const [allStudents, setAllStudents] = useState<StudentsType[]>([]);
   useEffect(() => {
-    const studentsFetch = async () => {
-      const studentsBody = await fetchAPI(
-        'https://server.manu-web.de/students'
-      );
-
-      setAllStudents(studentsBody);
-    };
-    studentsFetch();
+    setFetch('students', setAllStudents);
   }, []);
   return (
     <>
