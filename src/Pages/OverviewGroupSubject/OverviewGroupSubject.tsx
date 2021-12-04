8e734619c6dc909f139974c44d3e87c1ab58fc97
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-12-04 16:28:46 +0100

    :sparkles: Add OverviewGrouSubject page

---
+++ src/Pages/OverviewGroupSubject/OverviewGroupSubject.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import { GroupsType, SubjectsType } from '../../utils/types';
+import styles from './OverviewGroupSubject.module.css';
+import TitleElement from '../../Components/TitleElement/TitleElement';
+import EntryElement from '../../Components/EntryElement/EntryElement';
+import PullToRefresh from 'pulltorefreshjs';
+import { setGroupSubjectFetch } from '../../utils/helper';
+
+type OverviewSocialProps = {
+  category: 'groups' | 'subjects';
+};
+
+export default function OverviewSocial({
+  category,
+}: OverviewSocialProps): JSX.Element {
+  const [allEntries, setAllEntries] = useState<GroupsType[] | SubjectsType[]>(
+    []
+  );
+  console.log(allEntries);
+  PullToRefresh.init({
+    mainElement: 'div',
+    onRefresh() {
+      setGroupSubjectFetch(category, setAllEntries);
+    },
+  });
+
+  useEffect(() => {
+    setGroupSubjectFetch(category, setAllEntries);
+  }, []);
+  return (
+    <>
+      <TitleElement title={category} />
+      <div className={styles.results}>
+        {allEntries[0] ? (
+          allEntries.map((entry) => (
+            <EntryElement
+              key={entry.id}
+              category={category}
+              id={entry.id}
+              entryname={entry.name}
+            />
+          ))
+        ) : (
+          <span className={styles.noResult}>No entry found</span>
+        )}
+      </div>
+    </>
+  );
+}
