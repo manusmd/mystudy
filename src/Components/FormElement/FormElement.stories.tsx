a95bafc24cb90546e3ed4b51ca2ee0b402e0e2f0
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-12-07 13:33:20 +0100

    :sparkles: Add formElement files

---
+++ src/Components/FormElement/FormElement.stories.tsx
@@ -0,0 +1,8 @@
+import FormElement from './FormElement';
+
+export default {
+  component: FormElement,
+  title: 'Components/FormElement',
+};
+
+export const Default = () => <FormElement />;
---
+++ src/Components/FormElement/FormElement.tsx
@@ -0,0 +1,5 @@
+import styles from './CheckBoxElement.module.css';
+
+export default function FormElement(): JSX.Element {
+  return <h1>hallo</h1>;
+}
