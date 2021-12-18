24d7abce95e8b2d9cc083623335a5351036dc1e7
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-12-18 15:38:57 +0100

    :sparkles: Add logout function

--- src/Components/TitleElement/TitleElement.tsx
+++ src/Components/TitleElement/TitleElement.tsx
@@ -1,17 +1,24 @@
 import styles from './TitleElement.module.css';
 import logo from '../../assets/MyStudy.png';
 import AccountLogo from './assets/account';
+import { useNavigate } from 'react-router-dom';
 
 type TitleElementProps = {
   title: string;
 };
 
 export default function TitleElement({ title }: TitleElementProps) {
+  const navigate = useNavigate();
   return (
     <div className={styles.container}>
       <img className={styles.logo} src={logo} alt="" />
       <h2 className={styles.title}>{title}</h2>
-      <AccountLogo />
+      <AccountLogo
+        onClickHandler={() => {
+          sessionStorage.clear();
+          navigate('/');
+        }}
+      />
     </div>
   );
 }
